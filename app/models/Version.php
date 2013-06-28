967d87a5f2e1906ed0eb3588e0830ba55dbadf1e
Maxime Fabre <ehtnam6@gmail.com>
2013-06-29 01:19:02 +0200

    PSR fixes

--- app/models/Version.php
+++ app/models/Version.php
@@ -1,8 +1,15 @@
 <?php
 class Version extends Eloquent
 {
+
+	/**
+	 * Get the keywords of a Version
+	 *
+	 * @return array
+	 */
 	public function getKeywordsAttribute()
 	{
 		return json_decode($this->getOriginal('keywords'), true);
 	}
+
 }
