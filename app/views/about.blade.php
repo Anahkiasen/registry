e6c9f426a98bfef824a2a61936d567dfaf610ff4
Maxime Fabre <ehtnam6@gmail.com>
2013-07-01 15:30:57 +0200

    Add border to titles

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -22,4 +22,12 @@
 		<li>Favorites for the Packagist entry</li>
 	</ul>
 	<p>Each of these factors is then assigned a weight according to how important it is in the index</p>
+
+	<h2>Architecture</h2>
+	<p>
+		This registry binds the various APIs and services on which packages are hosted : Packagist, Github, Travis, Bitbucket.
+		As a lot of endpoints need to be hit in order to build a good registry, and as indexes need to be calculated,
+		the informations you see on the registry are not "live". The database is refreshed every night.
+	</p>
+	<p>The registry is a Laravel 4.1 application</p>
 @stop
