91a600ce11c5fb2878dafbf4592da72189483023
Maxime Fabre <ehtnam6@gmail.com>
2013-07-31 19:59:09 +0200

    Tweak tasks

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -10,14 +10,14 @@ Rocketeer::after(array('deploy', 'update'), function($task) {
 
 	$task->command->comment('Building Basset containers');
 	$task->runForCurrentRelease('php artisan basset:build -f -p');
-});
 
-Rocketeer::after('update', function($task) {
 	$task->command->comment('Updating database');
 	$task->remote->put(App::make('path').'/database/production.sqlite', $task->rocketeer->getFolder('shared/app/database/production.sqlite'));
 
 	$task->command->comment('Clearing cache');
 	$task->runForCurrentRelease('php artisan cache:clear && php artisan twig:clean');
+
+	$task->runForCurrentRelease('chown www-data app/storage/meta/collections.json');
 });
 
 Artisan::add(new Refresh);
