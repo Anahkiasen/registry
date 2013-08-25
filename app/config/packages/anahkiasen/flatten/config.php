1a7daecf4ff822267eb58ba9111f6d0c68512f3b
Maxime Fabre <ehtnam6@gmail.com>
2013-08-25 17:49:01 +0200

    Update Flatten

--- app/config/packages/anahkiasen/flatten/config.php
+++ app/config/packages/anahkiasen/flatten/config.php
@@ -1,11 +1,12 @@
 <?php return array(
 
+	// Cache checks
+	//
+	// Various checks that are used to see if Flatten should run
+	////////////////////////////////////////////////////////////////////
+
 	// The environments in which Flatten should not run
 	'environments' => array(),
-
-	// The default period during which a cached page should be kept (in minutes)
-	// 0 means the page never gets refreshed by itself
-	'lifetime'     => 0,
 
 	// The different pages to be ignored when caching
 	// They're all regexes so go crazy
@@ -16,9 +17,21 @@
 	// The ignored pages will still be substracted from this array
 	'only'         => array(),
 
+	// Here you can put variables that will be taken into account when
+	// checking if Flatten should run. If the sum of the array is not
+	// "true", then Flatten won't start
+	'blockers'     => array(),
+
+	// Cache variables
+	////////////////////////////////////////////////////////////////////
+
+	// The default period during which a cached page should be kept (in minutes)
+	// 0 means the page never gets refreshed by itself
+	'lifetime'     => 0,
+
 	// An array of string or variables to add to the salt being used
 	// to differentiate pages
 	'saltshaker'   => array(
-		Input::get('q'), Auth::check() ? Auth::user()->id : Auth::guest(),
+		Auth::check() ? Auth::user()->id : Auth::guest(),
 	),
 );
