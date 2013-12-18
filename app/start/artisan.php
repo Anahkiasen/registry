b4ba4068638f6f470fcf56b52e28656bf201d29f
Maxime Fabre <ehtnam6@gmail.com>
2013-12-18 15:11:35 +0100

    Move stats up in about page

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -6,7 +6,7 @@
 
 Rocketeer::after(array('deploy', 'update'), function($task) {
 	$task->command->comment('Building assets');
-	$task->runForCurrentRelease(['npm install', 'node node_modules/.bin/bower install', 'node node_modules/.bin/grunt production']);
+	$task->runForCurrentRelease(['npm install', 'node node_modules/.bin/bower install --allow-root', 'node node_modules/.bin/grunt production']);
 
 	$task->command->comment('Uploading database and setting permissions');
 	$task->remote->put(App::make('path').'/database/production.sqlite', $task->rocketeer->getFolder('shared/app/database/production.sqlite'));
