4b62108ba19d159998c156d0986388272322c3a4
Maxime Fabre <ehtnam6@gmail.com>
2014-01-11 15:27:57 +0100

    Switch version descriptions to TEXT too

--- app/database/migrations/2013_06_28_170949_create_versions.php
+++ app/database/migrations/2013_06_28_170949_create_versions.php
@@ -13,7 +13,7 @@ class CreateVersions extends Migration
 		Schema::create('versions', function($table) {
 			$table->increments('id');
 				$table->string('name');
-				$table->string('description');
+				$table->text('description');
 				$table->string('keywords');
 				$table->string('homepage');
 				$table->string('version');
