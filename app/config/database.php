d3529016ae74f0b88bc6823866ff19726f6411d4
Maxime Fabre <ehtnam6@gmail.com>
2014-01-11 15:27:17 +0100

    Switch to MySQL because the club can’t handle me

--- app/config/database.php
+++ app/config/database.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'default' => 'sqlite',
+	'default' => 'mysql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'registry',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
