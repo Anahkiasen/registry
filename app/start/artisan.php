638ff5de3ddf00c633401b5ddfd6a5c62f679c5a
Maxime Fabre <ehtnam6@gmail.com>
2014-01-19 15:18:59 +0100

    Add missing Rocketeer stuff

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -11,6 +11,9 @@ Rocketeer::after(array('deploy', 'update'), function ($task) {
 	$task->command->comment('Clearing cache');
 	$task->runForCurrentRelease('php artisan cache:clear && php artisan twig:clean');
 
+	$task->command->comment('Uploading database credentials');
+	$task->remote->put(App::make('path').'/config/production/database.php', $task->rocketeer->getFolder('current/config/production/database.php'));
+
 	$task->runForCurrentRelease('chown -R www-data:www-data storage');
 });
 
