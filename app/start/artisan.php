ddd4a0d5f853641d4f24739618488d69f953c75a
Maxime Fabre <ehtnam6@gmail.com>
2014-01-31 20:39:05 +0100

    Update to 4.1

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -1,21 +1,4 @@
 <?php
 
-//////////////////////////////////////////////////////////////////////
-///////////////////////////// DEPLOYMENT /////////////////////////////
-//////////////////////////////////////////////////////////////////////
-
-Rocketeer::after(array('deploy', 'update'), function ($task) {
-	$task->command->comment('Building assets');
-	$task->runForCurrentRelease(['npm install', 'node node_modules/.bin/bower install --allow-root', 'node node_modules/.bin/grunt production']);
-
-	$task->command->comment('Clearing cache');
-	$task->runForCurrentRelease('php artisan cache:clear && php artisan twig:clean');
-
-	$task->command->comment('Uploading database credentials');
-	$task->remote->put(App::make('path').'/config/production/database.php', $task->rocketeer->getFolder('current/config/production/database.php'));
-
-	$task->runForCurrentRelease('chown -R www-data:www-data storage');
-});
-
 Artisan::add(new Refresh);
 Artisan::add(new Update);
