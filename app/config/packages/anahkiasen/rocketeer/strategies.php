86f2617f24f8d4ef0f2c1208691fe832e50ecace
Maxime Fabre <ehtnam6@gmail.com>
2014-08-15 16:51:23 +0200

    Update Rocketeer configuration

---
+++ app/config/packages/anahkiasen/rocketeer/strategies.php
@@ -0,0 +1,36 @@
+<?php
+use Rocketeer\Binaries\Composer;
+
+return array(
+
+	// Task strategies
+	//
+	// Here you can configure in a modular way which tasks to use to
+	// execute various core parts of your deployment's flow
+	//////////////////////////////////////////////////////////////////////
+
+	// Which strategy to use to create a new release
+	'deploy'       => 'Clone',
+
+	// Which strategy to use to test your application
+	'test'         => 'Phpunit',
+
+	// Which strategy to use to migrate your database
+	'migrate'      => 'Artisan',
+
+	// Which strategy to use to install your application's dependencies
+	'dependencies' => 'Polyglot',
+
+	// Execution hooks
+	//////////////////////////////////////////////////////////////////////
+
+	'composer' => array(
+		'install' => function (Composer $composer, $task) {
+			return $composer->install([], ['--no-interaction' => null, '--no-dev' => null, '--prefer-dist' => null]);
+		},
+		'update' => function (Composer $composer) {
+			return $composer->update();
+		},
+	),
+
+);
